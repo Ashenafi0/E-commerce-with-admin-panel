1dc36442a8c5d972b536a4799c02f729cbb02dc4
Ashenafi0 <esayasashenafi21@gmail.com>
2025-03-19 11:27:08 +0300

    New changes for main branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      <h1> Header component</h1>
+      <h1> Header components</h1>
     </div>
   );
 }
