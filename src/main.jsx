6d995d74fcae85a71cc2c28dd242f99a787480ca
Ashenafi0 <esayasashenafi21@gmail.com>
2025-03-22 16:29:18 +0300

    Updated some changes

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom"; // We need to import the BrowserRouter component from the react-router-dom package
+import { Provider } from "react-redux"; // This is the Provider component that we need to wrap the App component with
+import store from "./store/store.js"; // This is the Redux store that we created in the previous step
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
